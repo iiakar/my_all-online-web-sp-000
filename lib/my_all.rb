160c2eecf3771b50cb22fc9405c9ff88db70363a
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 15:23:53 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,10 +8,10 @@ def my_all?(collection)
   yield(collection[i])
   i = i + 1 
   end
-  # if 
-  #   block_return_values.includes?(false)
-  #     false
-  #   else
-  #     true
-  #   end
+  if 
+    block_return_values.include?(false)
+      false
+    else
+      true
+    end
 end
